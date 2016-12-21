bd185d68b815b0e37d43fa22782b95e4294fbcae
Jon Bukiewicz <jbukiewicz@devnext.net>
2016-12-21 07:15:58 -0600

    additional work on states, sharing data between states

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,49 @@ import React from 'react';
 import Header from './Header'; // Already in App.js, pulling sibling file
 import Order from './Order'; 
 import Inventory from './Inventory'; 
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+	constructor(){
+		super();
+		// Cannot call this without super()
 
-	// Remember, at least one method
+		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
+
+		// Initial state - getinitialState()
+		this.state = {
+			fishes: {},
+			order: {}
+		}
+	}
+
+	addFish(fish) {
+		// update our state
+			// you can directly update through this.state.fishes.fish1 = fish
+			// however, it is best practice to make a copy and then update, for performance
+
+			// our existing fishes state
+			// using es6 spread
+			const fishes = {...this.state.fishes};
+
+			// add in our new fish
+			const timestamp = Date.now();
+			fishes[`fish-${timestamp}`] = fish;
+
+
+		// set state
+		this.setState({ fishes // same as fishes: fishes 
+		});
+	}
+
+	loadSamples() {
+		this.setState({
+			fishes: sampleFishes
+		})
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -15,9 +54,16 @@ class App extends React.Component {
 						Note that we can pass data to a component through props: age="500" cool={true} tagline="Lorem Ipsum", ec
 					*/}
 					<Header tagline="Fresh Seafood Market" />
+					<ul className="list-of-fishes">
+						{
+							Object
+								.keys(this.state.fishes)
+								.map(key => <Fish key={key} />)
+						}
+					</ul>
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		)
 	}
---
+++ src/components/Fish.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Fish extends React.Component {
+	render(){
+		return (
+			<li className="menu-fish"> </li>
+		)
+	}
+}
+
+export default Fish;
